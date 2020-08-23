f8ee2702a5614d7fd6d1dbacf7773d03a6af791d
Tamas Hajdu <tamas.hajdu@ericsson.com>
2020-08-23 11:16:41 +0200

    - modified README - code refactorization - fixed testcases

--- get_ip_addresses.py
+++ get_ip_addresses.py
@@ -3,6 +3,7 @@
 import argparse
 import os
 import subprocess
+import sys
 
 """
     Script to get all IP addresses.
@@ -10,11 +11,17 @@ import subprocess
     --overlapping prints the overlapping subnets from the IP addresses
 """
 
+IPS_WITH_PREFIX = "ip -o -f inet addr show | awk '/scope global/ {print $4}'"
+IPS_ALL = "ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*'"
 
 class IpAddresses:
 
     def get_ip_addresses(self):
-        args = self.parse_arguments()
+        '''
+        The main function to execute. If one of the shell commands fails to execute, the programs throws
+        a RuntimeError exception.
+        '''
+        args = self.parse_arguments(sys.argv[1:])
         try:
             if args.overlapping:
                 return self.ip_addresses_overlapping()
@@ -29,27 +36,27 @@ class IpAddresses:
         '''
         Getting all the IPs.
         '''
-        ip_addresses_all = os.system("ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' |"
-                                     " grep -Eo '([0-9]*\.){3}[0-9]*'")
+        ip_addresses_all = os.system(IPS_ALL)
         return ip_addresses_all
 
     def ip_addresses_with_prefix(self):
         '''
         Getting the list of IP addresses, with subnets.
         '''
-        ip_addresses_with_subnet = os.system("ip -o -f inet addr show | awk '/scope global/ {print $4}'")
+        ip_addresses_with_subnet = os.system(IPS_WITH_PREFIX)
         return ip_addresses_with_subnet
 
     def ip_addresses_overlapping(self):
         '''
         Getting the list of IP addresses, then printing the overlapping addresses
         '''
-        ip_addresses = subprocess.check_output("ip -o -f inet addr show | awk '/scope global/ {print $4}'", shell=True)
+        ip_addresses = subprocess.check_output(IPS_WITH_PREFIX, shell=True)
         addresses_string_parameter = ' '.join(ip_addresses.decode("utf-8").split('\n'))
         overlapping_ips = os.system("ipconflict -o " + addresses_string_parameter)
         return overlapping_ips
 
-    def parse_arguments(self):
+    def parse_arguments(self,
+                        args):
         parser = argparse.ArgumentParser(
             prog="get_ip_addresses",
             usage=None,
@@ -68,7 +75,7 @@ class IpAddresses:
             "--overlapping",
             action='store_true'
         )
-        args = parser.parse_args()
+        args = parser.parse_args(args)
         return args
 
 
--- test_get_ip_addresses.py
+++ test_get_ip_addresses.py
@@ -1,6 +1,6 @@
+import argparse
 import subprocess
 import unittest.mock
-from unittest.mock import patch
 from argparse import Namespace
 from get_ip_addresses import IpAddresses
 
@@ -11,23 +11,20 @@ class TestStringMethods(unittest.TestCase):
         self.ip_addresses_all = IpAddresses().ip_addresses_all()
         self.ip_addresses_with_prefix = IpAddresses().ip_addresses_with_prefix()
         self.ip_addresses_overlapping = IpAddresses().ip_addresses_overlapping()
-        # self.parser = IpAddresses().parse_arguments()
         self.maxDiff = None
 
-    @patch('os.system')
-    @patch('get_ip_addresses.IpAddresses.parse_arguments')
+    @unittest.mock.patch('os.system')
+    @unittest.mock.patch('get_ip_addresses.IpAddresses.parse_arguments')
     def test_get_ip_addresses_all(self,
                                   mock_parse,
                                   mock_bash_command):
         mock_parse.return_value = Namespace(overlapping=False, with_prefix=False)
         IpAddresses().ip_addresses_all()
-        bash_command = "ifconfig | grep" \
-                       " -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' |" \
-                       " grep -Eo '([0-9]*\.){3}[0-9]*'"
+        bash_command = "ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*'"
         mock_bash_command.assert_called_once_with(bash_command)
 
     @unittest.mock.patch('os.system')
-    @patch('get_ip_addresses.IpAddresses.parse_arguments')
+    @unittest.mock.patch('get_ip_addresses.IpAddresses.parse_arguments')
     def test_ip_addresses_with_prefix(self,
                                       mock_parse,
                                       mock_bash_command):
@@ -37,17 +34,17 @@ class TestStringMethods(unittest.TestCase):
         mock_bash_command.assert_called_once_with(bash_command)
 
     @unittest.mock.patch('os.system')
-    @patch('get_ip_addresses.IpAddresses.parse_arguments')
+    @unittest.mock.patch('get_ip_addresses.IpAddresses.parse_arguments')
     def test_ip_addresses_overlapping(self,
                                       mock_parse,
                                       mock_bash_command):
         mock_parse.return_value = Namespace(overlapping=True, with_prefix=False)
-        IpAddresses().ip_addresses_with_prefix()
-        bash_command = "ip -o -f inet addr show | awk '/scope global/ {print $4}'"
+        IpAddresses().ip_addresses_overlapping()
+        bash_command = "ipconflict -o 192.168.0.6/24 100.109.0.2/24 "
         mock_bash_command.assert_called_once_with(bash_command)
 
-    @patch('get_ip_addresses.IpAddresses.ip_addresses_all')
-    @patch('get_ip_addresses.IpAddresses.parse_arguments')
+    @unittest.mock.patch('get_ip_addresses.IpAddresses.ip_addresses_all')
+    @unittest.mock.patch('get_ip_addresses.IpAddresses.parse_arguments')
     def test_no_parameters_called(self,
                                   mock_parse,
                                   mock_all):
@@ -55,8 +52,8 @@ class TestStringMethods(unittest.TestCase):
         mock_all.return_value = "0.0.0.0"
         self.assertEqual("0.0.0.0", IpAddresses().get_ip_addresses())
 
-    @patch('get_ip_addresses.IpAddresses.ip_addresses_with_prefix')
-    @patch('get_ip_addresses.IpAddresses.parse_arguments')
+    @unittest.mock.patch('get_ip_addresses.IpAddresses.ip_addresses_with_prefix')
+    @unittest.mock.patch('get_ip_addresses.IpAddresses.parse_arguments')
     def test_with_prefix_called(self,
                                 mock_parse,
                                 mock_prefix):
@@ -64,8 +61,8 @@ class TestStringMethods(unittest.TestCase):
         mock_prefix.return_value = "0.0.0.0"
         self.assertEqual("0.0.0.0", IpAddresses().get_ip_addresses())
 
-    @patch('get_ip_addresses.IpAddresses.ip_addresses_overlapping')
-    @patch('get_ip_addresses.IpAddresses.parse_arguments')
+    @unittest.mock.patch('get_ip_addresses.IpAddresses.ip_addresses_overlapping')
+    @unittest.mock.patch('get_ip_addresses.IpAddresses.parse_arguments')
     def test_overlapping_called(self,
                                 mock_parse,
                                 mock_prefix):
@@ -73,8 +70,8 @@ class TestStringMethods(unittest.TestCase):
         mock_prefix.return_value = "0.0.0.0"
         self.assertEqual("0.0.0.0", IpAddresses().get_ip_addresses())
 
-    @patch('get_ip_addresses.IpAddresses.ip_addresses_all')
-    @patch('get_ip_addresses.IpAddresses.parse_arguments')
+    @unittest.mock.patch('get_ip_addresses.IpAddresses.ip_addresses_all')
+    @unittest.mock.patch('get_ip_addresses.IpAddresses.parse_arguments')
     def test_raise_runtime_error(self,
                                  mock_parse,
                                  mock_all):
@@ -83,7 +80,7 @@ class TestStringMethods(unittest.TestCase):
         self.assertRaises(RuntimeError, IpAddresses().get_ip_addresses)
 
     def test_parser(self):
-        parser = IpAddresses.parse_arguments(['--with-prefix', '--overlapping'])
+        parser = IpAddresses().parse_arguments(args=["--with-prefix", "--overlapping"])
         self.assertTrue(parser)
 
     def tearDown(self):
